e255eba4a2b569793be3f1a08dcd99117dfabba6
yamuna <yamunadevi.g@sendmygift.com>
2016-04-25 11:25:43 +0530

    Commit

---
+++ app/src/main/java/com/example/user/smgapp/ExpandableMenuModel.java
@@ -0,0 +1,25 @@
+package com.example.user.smgapp;
+
+/**
+ * Created by Admin on 20-10-2015.
+ */
+public class ExpandableMenuModel {
+    String iconName = "";
+    int iconImg = -1; // menu icon resource id
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+
+    public int getIconImg() {
+        return iconImg;
+    }
+
+    public void setIconImg(int iconImg) {
+        this.iconImg = iconImg;
+    }
+}
