4a2c2a07516774cb740906b80af87664be9815de
chinchu_smg <chinchumariapaul@gmail.com>
2016-04-25 11:32:29 +0530

    chinchu

---
+++ app/src/main/java/com/example/user/smgapp/GlobalClass.java
@@ -0,0 +1,19 @@
+package com.example.user.smgapp;
+
+import android.app.Application;
+import android.graphics.Typeface;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GlobalClass extends Application {
+
+
+    public String Selected_Category="ProductList";
+
+
+
+
+
+
+}
